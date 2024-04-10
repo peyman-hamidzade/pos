148eef7d0b8fd65613fe04c319e5f657f118d500
peyman <peymanh685@gmail.com>
2024-04-10 20:51:45 +0330

    update requirements.txt

--- frontend/pos/src/components/axiosInstance/axiosInstance.js
+++ frontend/pos/src/components/axiosInstance/axiosInstance.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const axiosInstance = axios.create({
 
 
-  baseURL: 'http://127.0.0.1:8000/api/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://epasargad.liara.run/api/',
 });
 
 
