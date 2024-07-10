5e689257cd470b191181ae5b81d12d41c77fdfbd
peyman <peymanh685@gmail.com>
2024-07-11 00:05:48 +0330

    cart head

--- frontend/pos/src/components/cart/CartComponent.js
+++ frontend/pos/src/components/cart/CartComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Coupon from '../coupon/Coupon';
 
 const CartComponent = () => {
@@ -57,6 +57,36 @@ const CartComponent = () => {
     };
 
     return (
+        <>
+        <section className="px-4 mb-24">
+        <div className="container mx-auto max-w-screen-xl">
+            <nav className="flex mb-5 border-y border-orange-200 py-3" aria-label="Breadcrumb">
+                <ol className="inline-flex items-center space-x-1 md:space-x-2">
+                    <li className="inline-flex items-center">
+                        <Link to={'/'} className="inline-flex items-center">
+                        خانه
+                        </Link>
+                    </li>
+                    <li>
+                        <div className="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                            </svg>
+                            
+                            <Link to={''} className="mr-1 text-sm font-medium">سبد خرید</Link>
+                        </div>
+                    </li>
+                </ol>
+            </nav>  
+            <div className="flex justify-center relative my-16">
+                <h2 className="font-YekanBakh-ExtraBlack text-3xl">ســبــد خــریــد</h2>
+                <div className="absolute -top-6">
+                    <span className="font-YekanBakh-ExtraBlack text-6xl text-opacity-10 text-stone-900">cart</span>
+
+                </div>
+                <div className="bg-orange-200 w-20 h-1.5 rounded-full absolute top-10"></div>
+
+            </div> 
         <div className="cart-container">
             <table className="cart-table">
                 <thead>
@@ -96,6 +126,9 @@ const CartComponent = () => {
                 </div>
             </div>
         </div>
+        </div>
+        </section>
+        </>
     );
 };
 
